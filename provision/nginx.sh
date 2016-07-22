26904d914856cc841df908b60141bf6e2d99035e
Cecile Muller <cecilemuller@users.noreply.github.com>
2016-07-23 00:06:55 +0200

    Nginx provision can setup multiple sites now
    It looks for all .conf files in `/box/sites-available` instead of only testing for `/box/sites-available/www.conf`

--- provision/nginx.sh
+++ provision/nginx.sh
@@ -4,19 +4,41 @@ dpkg -s "nginx" >/dev/null 2>&1 && {
 	echo "INFO: Nginx is already installed"
 } || {
 	echo "INFO: Installing Nginx..."
+
 	sudo adduser ubuntu www-data
 	sudo apt-get install nginx -y > /dev/null
 }
 
-if [ -f "/box/sites-available/www.conf" ]; then
-	echo "INFO: Setting up Virtual Hosts"
-	sudo systemctl stop nginx
-	if [ -f "/etc/nginx/sites-enabled/default" ]; then
-		sudo rm /etc/nginx/sites-enabled/default
+
+# Makes sure we loop over real files only
+shopt -s nullglob
+
+has_sites=false
+for filename in /box/sites-available/*.conf; do
+	has_sites=true
+	break
+done
+
+
+if [ "$has_sites" = true ]; then
+	echo "INFO: Setting up Nginx sites..."
+
+	# Removes existing sites
+	sudo rm -rf /etc/nginx/sites-enabled/*
+	sudo rm -rf /etc/nginx/sites-available/*
+
+	# Adds the site configs from the box
+	for filename in /box/sites-available/*.conf; do
+		name=`basename "$filename"`
+		sudo cp /box/sites-available/$name /etc/nginx/sites-available/$name
+		sudo ln -s /etc/nginx/sites-available/$name /etc/nginx/sites-enabled/$name
+	done
+
+	# Makes sure Nginx is running, and uses the new configs
+	nginx_active=`systemctl is-active nginx >/dev/null 2>&1 && echo YES || echo NO`
+	if [ $nginx_active = "YES" ]; then
+		sudo systemctl reload nginx
+	else
+		sudo systemctl start nginx
 	fi
-	sudo cp /box/sites-available/www.conf /etc/nginx/sites-available/www.conf --force
-	if [ ! -f "/etc/nginx/sites-enabled/www.conf" ]; then
-		sudo ln -s /etc/nginx/sites-available/www.conf /etc/nginx/sites-enabled/www.conf
-	fi
-	sudo systemctl start nginx
 fi
