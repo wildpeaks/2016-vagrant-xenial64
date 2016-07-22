85ca03920019b68cb57debc0c02ec9e4727549f2
Cecile Muller <git@wildpeaks.fr>
2016-07-22 10:13:53 +0200

    Added Nginx provision script

--- provision/nginx.sh
+++ provision/nginx.sh
@@ -8,19 +8,11 @@ dpkg -s "nginx" >/dev/null 2>&1 && {
 	sudo adduser ubuntu www-data
 	sudo apt-get install nginx -y > /dev/null
 
-	## Only online needs to set ownership and permissions
-	# chown -R ubuntu:www-data /var/www/*
-	# chmod -R 775 /var/www/*
-
-	#
-	# TODO there might be multiple sites on the same box though
-	#
-	if [ -f "/setup/sites-available/www.conf" ]; then
+	if [ -f "/box/sites-available/www.conf" ]; then
 		sudo systemctl stop nginx
 		sudo rm /etc/nginx/sites-enabled/default
-		sudo cp /setup/sites-available/www.conf /etc/nginx/sites-available/www.conf
+		sudo cp /box/sites-available/www.conf /etc/nginx/sites-available/www.conf
 		sudo ln -s /etc/nginx/sites-available/www.conf /etc/nginx/sites-enabled/www.conf
-		## sudo ln -fs /etc/nginx/sites-available/www.conf /etc/nginx/sites-enabled/www.conf
 		sudo systemctl start nginx
 	fi
 }
