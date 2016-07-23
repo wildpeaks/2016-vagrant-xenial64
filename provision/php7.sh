fee5f637c12b74ad85bfbb1f1534b93fc8ab8d69
Cecile Muller <git@wildpeaks.fr>
2016-07-23 15:01:16 +0200

    PHP provision can optionally install php7.0-pgsql
    If it detects that PostgreSQL 9.5 is installed

--- provision/php7.sh
+++ provision/php7.sh
@@ -4,6 +4,9 @@ dpkg -s "php7.0-fpm" >/dev/null 2>&1 && {
 	echo "INFO: PHP 7 is already installed"
 } || {
 	echo "INFO: Installing PHP 7..."
-	sudo apt-get install php7.0-fpm -y > /dev/null
-	sudo apt-get install php7.0-curl php7.0-json -y > /dev/null
+	dpkg -s "postgresql-9.5" >/dev/null 2>&1 && {
+		sudo apt-get install php7.0-fpm php7.0-curl php7.0-json php7.0-pgsql -y > /dev/null
+	} || {
+		sudo apt-get install php7.0-fpm php7.0-curl php7.0-json -y > /dev/null
+	}
 }
