e69c665a648bd24cda919605b67ceb1e71ef28b9
Cecile Muller <git@wildpeaks.fr>
2016-07-23 14:29:04 +0200

    Added Neo4j provision script
    Surprisingly, despite the weak SHA-1 key issue (see
    https://github.com/neo4j/neo4j/issues/7052 ), it installs just fine
    :smile:

--- provision/neo4j.sh
+++ provision/neo4j.sh
@@ -7,5 +7,6 @@ dpkg -s "neo4j" >/dev/null 2>&1 && {
 	wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 	echo 'deb http://debian.neo4j.org/repo stable/' > /tmp/neo4j.list
 	sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
+	sudo apt-get update
 	sudo apt-get install neo4j -y > /dev/null
 }
