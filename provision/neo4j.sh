cf4537e5217bf4de46d07df2bbfdca82e11c896d
Cecile Muller <git@wildpeaks.fr>
2016-07-23 13:27:43 +0200

    Fixed line endings

---
+++ provision/neo4j.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dpkg -s "neo4j" >/dev/null 2>&1 && {
+	echo "INFO: Neo4j is already installed"
+} || {
+	echo "INFO: Installing Neo4j..."
+	wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+	echo 'deb http://debian.neo4j.org/repo stable/' > /tmp/neo4j.list
+	sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
+	sudo apt-get install neo4j -y > /dev/null
+}
